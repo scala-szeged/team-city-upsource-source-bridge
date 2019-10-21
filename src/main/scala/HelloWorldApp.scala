f1bb45f2ca7b296141d02c20d0b29d8b21a3f390
Setény János <seteny.janos@tigra.hu>
2019-10-21 14:42:17 +0200

    Try to listen on port 63330 from Nodejs

--- src/main/scala/HelloWorldApp.scala
+++ src/main/scala/HelloWorldApp.scala
@@ -1,13 +1,23 @@
 import io.scalajs.nodejs._
+import io.scalajs.nodejs.http.Http._
+import io.scalajs.nodejs.http.{ClientRequest, ServerResponse}
 
 object HelloWorldApp {
   def main(): Unit = {
     println("Hello world from Scala.js!")
+
     println("----")
     println("To demonstrate usage of Node.js api, here's some of your environment:")
     process.env.take(5).foreach {
       case (key, value) =>
         println(s"$key = $value")
     }
+
+    println("----")
+    createServer { (req: ClientRequest, res: ServerResponse) =>
+      res.write("Ola!")
+      res.end()
+    }.listen(63330)
+    println("Listening on port 63330")
   }
 }
