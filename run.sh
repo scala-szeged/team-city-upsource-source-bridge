0ca6ecb1e3548b34245e6d6fd282109c81086cf9
Setény János <seteny.janos@tigra.hu>
2019-10-22 09:50:06 +0200

    Use the UPSOURCE_URL environment variable

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-node target/scala-2.12/scalajs-bundler/main/team-city-upsource-south-bridge-fastopt.js
+node target/scala-2.12/scalajs-bundler/main/team-city-upsource-source-bridge-fastopt.js
--- src/main/scala/TeamCityUpsourceSouthBridge.scala
+++ src/main/scala/TeamCityUpsourceSouthBridge.scala
@@ -1,5 +1,6 @@
 import io.scalajs.nodejs.http.Http._
 import io.scalajs.nodejs.http.{IncomingMessage, ServerResponse}
+import io.scalajs.nodejs.process
 
 import scala.util.{Failure, Success}
 
@@ -7,11 +8,17 @@ object TeamCityUpsourceSouthBridge {
   def main(): Unit = {
     println("TeamCityUpsourceSouthBridge")
 
+    //      val base = "http://ocmaster.szeged.tigra.hu:8085/aeek-telemed-core-dr/file/ec3b67263dd7d3bb08bc9f520fcd6f1929226dfa/"
+    val base = process.env.find { case (key, _) =>
+      key == "UPSOURCE_URL"
+    }.getOrElse {
+      throw new Exception("Please provide the Upsource base url in the UPSOURCE_URL environment variable.")
+    }._2 // value
+
     createServer { (req: IncomingMessage, res: ServerResponse) =>
       println(req.url)
 
       // http://ocmaster.szeged.tigra.hu:8085/aeek-telemed-core-dr/file/ec3b67263dd7d3bb08bc9f520fcd6f1929226dfa/src/main/java/hu/lippert/telemed/data/Application.java?nav=1014:1020:focused&line=0&preview=false
-      val base = "http://ocmaster.szeged.tigra.hu:8085/aeek-telemed-core-dr/file/ec3b67263dd7d3bb08bc9f520fcd6f1929226dfa/"
       val file = req.url.split("&").filter(_.startsWith("file=")).head.drop("file=".size)
       val url = base + file + "?nav=714:720:focused&line=0&preview=false"
 
